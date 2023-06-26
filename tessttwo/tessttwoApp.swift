b9b7620dc19f9f35b9f53a2d88962fdd664c4bc6
Lane Martin <Lanemartin2505@gmail.com>
2023-06-26 15:27:37 -0400

    Initial Commit

---
+++ tessttwo/tessttwoApp.swift
@@ -0,0 +1,17 @@
+//
+//  tessttwoApp.swift
+//  tessttwo
+//
+//  Created by Scholar on 6/26/23.
+//
+
+import SwiftUI
+
+@main
+struct tessttwoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
